adc771e371f867619e7a7953f378966e6a4c56f8
David Riccitelli <david@insideout.io>
2016-11-30 18:41:46 +0200

    add travis integration

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+
+const config = {
+    entry: './js/playground.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        // new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'})
+    ]
+};
+
+module.exports = config;
