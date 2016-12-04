61860ed4dfa93a838eaa1c63f10cc2ff0194610e
David Riccitelli <david@insideout.io>
2016-12-04 20:00:05 +0200

    use SauceLabs

--- karma.conf.js
+++ karma.conf.js
@@ -3,6 +3,11 @@
 
 module.exports = function (config) {
 
+    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+        console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+        process.exit(1)
+    }
+
     // Example set of browsers to run on Sauce Labs
     // Check out https://saucelabs.com/platforms for all browser/platform combos
     var customLaunchers = {
@@ -32,78 +37,30 @@ module.exports = function (config) {
     };
 
     config.set({
-
-        // base path that will be used to resolve all patterns (eg. files, exclude)
         basePath: '',
-
-
-        // frameworks to use
-        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
         frameworks: ['jasmine'],
-
-
-        // list of files / patterns to load in the browser
         files: [
-            'test/**/*Spec.js'
+            'src/*.js',
+            'test/*.js'
         ],
-
-
-        // list of files to exclude
-        exclude: [],
-
-
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {},
-
-
-        // test results reporter to use
-        // possible values: 'dots', 'progress'
-        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        // reporters: ['progress'],
-
-
-        // web server port
+        reporters: ['progress', 'saucelabs'],
         port: 9876,
-
-
-        // enable / disable colors in the output (reporters and logs)
         colors: true,
-
-
-        // level of logging
-        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-        logLevel: config.LOG_INFO,
-
-
-        // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: true,
-
-
-        // start these browsers
-        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: ['Chrome'],
-
-
-        // Continuous Integration mode
-        // if true, Karma captures browsers, runs the tests and exits
-        // singleRun: false,
-
-        // Concurrency level
-        // how many browser should be started simultaneous
-        concurrency: Infinity,
-
-        // The rest of your karma config is here
-        // ...
         sauceLabs: {
-            testName: 'Web App Unit Tests'
+            testName: 'Karma and Sauce Labs demo',
+            recordScreenshots: true,
+            connectOptions: {
+                port: 5757,
+                logfile: 'sauce_connect.log'
+            },
+            public: 'public'
         },
+        // Increase timeout in case connection in CI is slow
+        captureTimeout: 120000,
         customLaunchers: customLaunchers,
         browsers: Object.keys(customLaunchers),
-        reporters: ['dots', 'saucelabs'],
         singleRun: true
-    })
-}
-)
-}
-;
+    });
+
+};
+
