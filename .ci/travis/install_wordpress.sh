cb62f7506cd7c95ac8850c2a48904837225fed7b
David Riccitelli <david@insideout.io>
2016-12-06 00:13:13 +0200

    test wdio

--- .ci/travis/install_wordpress.sh
+++ .ci/travis/install_wordpress.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-cd %TRAVIS_BUILD_DIR%
 wp core download
 wp core config --dbname=wordpress --dbuser=root
 wp core install --url=localhost --title=WordPress --admin_user=admin --admin_password=admin --admin_email=admin@example.org
