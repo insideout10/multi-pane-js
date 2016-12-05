b52815dae25c780922350c209fd4bf4d4ef8397a
David Riccitelli <david@insideout.io>
2016-12-05 22:22:35 +0200

    try having SauceLabs connect to WP

--- .ci/travis/install_wordpress.sh
+++ .ci/travis/install_wordpress.sh
@@ -2,4 +2,4 @@
 
 wp core download
 wp core config --dbname=wordpress --dbuser=root
-wp core install --url=localhost --title=WordPress --admin_user=admin --admin_password=admin --admin_email=admin@localhost
+wp core install --url=localhost --title=WordPress --admin_user=admin --admin_password=admin --admin_email=admin@example.org
