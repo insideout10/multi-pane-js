3837c9594b2183d2c4397888ac40865536630dc8
David Riccitelli <david@insideout.io>
2016-12-05 23:12:06 +0200

    test wdio

---
+++ .ci/travis/install_nodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# See https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs
+npm install -g npm
