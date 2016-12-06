34b4952bbe9e89a2fdd5994d241e292b6a227303
David Riccitelli <david@insideout.io>
2016-12-06 17:34:16 +0200

    add tests to reach the plugin page

--- .ci/travis/install_wordpress.sh
+++ .ci/travis/install_wordpress.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd htdocs
 wp core download
 wp core config --dbname=wordpress --dbuser=root
 wp core install --url=localhost --title=WordPress --admin_user=admin --admin_password=admin --admin_email=admin@example.org
